5db3969a7a11864aeff162166600b45ffeb59c35
dbha <dbha0719@gmail.com>
2022-07-22 15:39:57 +0900

    update

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello Hello Hello\n');
+  res.send('Nodejs Hello\n');
 });
 
 app.listen(PORT, HOST);
