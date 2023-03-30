9955ef0ad652d3079f1389517a9b860835057316
dbha <dbha@tapdemo.com>
2023-03-30 11:43:47 +0900

    update

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Nodejs Hello TBS!!\n');
+  res.send('Hell NodeJS!!\n');
 });
 
 app.listen(PORT, HOST);
